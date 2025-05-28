90e6757b2a982f936f3d1927d47a4a5296ee739c
Ryan Mohammadian <ryanm@MacBook-Pro-318.local>
2025-05-27 22:59:59 -0500

    Initial Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function Home() {
+    const [clicked, setClick] = useState(false);
+
+    function handleClick() {
+        if (clicked)
+            setClick(false);
+        else
+            setClick(true);
+    }
+    return (
+        <div>
+            {clicked ?
+                <h1>Workout Created</h1>
+            : <h1>Create Workout</h1>}
+            <h1>Welcome to RepScape</h1>
+            <button onClick={handleClick}>Create Workout</button>
+        </div>
+    );
+}
+
+export default Home;
--- src/pages/Workout.js
+++ src/pages/Workout.js
@@ -1,1 +1,5 @@
-a
+function Workout() {
+    return <h1>Click here to start a workout!</h1>;
+}
+
+export default Workout;
