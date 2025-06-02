c219bd6d59036820a25cae99700387276bb8c9c8
Ryan Mohammadian <ryanm@MacBook-Pro-318.local>
2025-06-01 23:37:36 -0500

    Set up RepList

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,80 @@
 import React, { useState } from "react";
 
 function Home() {
-    const [clicked, setClick] = useState(false);
+    const [text, setText] = useState("");
+    const [repText, setRepText] = useState("");
+    const [list, setList] = useState([]);
+    //const [repList, setRepList] = useState([]);
 
-    function handleClick() {
-        if (clicked)
-            setClick(false);
-        else
-            setClick(true);
-    }
+
+    const handleText = (event) => {
+        setText(event.target.value);
+    };
+
+    /*const handleRepText = (event) => {
+        setRepText(event.target.value);
+    };*/
+
+    const addToList = () => {
+        if (text.trim === "") return;
+        setList([...list, text]);
+        setText("");
+    };
+
+    /*const addToRepList = () => {
+        if (text.trim === "") return;
+        setRepList([...repList, text]);
+        setText("");
+    };*/
+
+    const deleteFromList = (indexToDelete) => {
+        const newItems = list.filter((_, index) => index !== indexToDelete);
+        setList(newItems);
+    };
+
+    /*const deleteFromRepList = (indexToDelete) => {
+        const newItems = repList.filter((_, index) => index !== indexToDelete);
+        setList(newItems);
+    };*/
+
     return (
         <div>
-            {clicked ?
-                <h1>Workout was Created</h1>
-            : <h1>Create Workout</h1>}
-            <h1>Welcome to RepScape</h1>
-            <button onClick={handleClick}>Create Workout</button>
+            <h1>Welcome to SetWork!</h1>
+            {/*{clicked ?
+            <h1>You Added: {text}!</h1>
+            : <h1>No Exercises</h1>}*/}
+            <h2>Insert Exercise Name: </h2>
+            <input 
+                type="text"
+                value={text}
+                onChange={handleText}
+                placeholder="Enter text here"
+            />
+            <button onClick={addToList}>Add</button>
+
+            <ul>
+                {list.map((item, index) => (
+                    <li key={index}>
+                        {item}
+                        <button onClick={() => deleteFromList(index)}>❌</button>
+                        {/*<ul>
+                            <h2>How many reps?: </h2>
+                            <input
+                                type="text"
+                                value={repText}
+                                onChange={handleRepText}
+                                placeholder="Enter text here"
+                            />
+                            <button onClick={addToRepList}>Add</button>
+                            {repList.map((item, index) => (
+                                <li key={index}>
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>*/}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
